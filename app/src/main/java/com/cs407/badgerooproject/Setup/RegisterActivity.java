161157d1e4d8875eb99e3f7af8d5de956de9e9cc
Andrea Tseng <htseng23@wisc.edu>
2023-12-02 21:53:56 -0600

    Setup database table and finished register activity

--- app/src/main/java/com/cs407/badgerooproject/Setup/RegisterActivity.java
+++ app/src/main/java/com/cs407/badgerooproject/Setup/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cs407.badgerooproject.R;
 
@@ -13,17 +15,33 @@ import com.cs407.badgerooproject.R;
 
 public class RegisterActivity extends AppCompatActivity {
     Button reg_btn;
+    EditText email_edt;
+    EditText password_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email_edt = findViewById(R.id.email_edt);
+        password_edt = findViewById(R.id.passwrod_edt);
         reg_btn = findViewById(R.id.register_btn);
+
+        //Initialize database
+        DBHelper dbHelper = new DBHelper(this);
         reg_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this, UploadProfilePicture.class);
-                startActivity(intent);
+                String email = email_edt.getText().toString();
+                String password = password_edt.getText().toString().trim();
+
+                if (email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Please enter your email and password", Toast.LENGTH_LONG).show();
+                }else{
+                    dbHelper.insertLoginCredentials(email, password);
+                    Intent intent = new Intent(RegisterActivity.this, UploadProfilePicture.class);
+                    startActivity(intent);
+                }
             }
         });
 
